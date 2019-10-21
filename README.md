4bab08c4e4e58b22556c6802056f90446cb074da
ronnieshum <hokyinshum@gmail.com>
2019-10-21 16:24:48 -0700

    Add README.md

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# location_stream
+# Routine Home & Work
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
+
+## Prerequisites
+Written in Python 2.7. Numpys, Pandas, and Scikit-learn needed.
+
+##Problem Statement
+Given a stream of location history determine the location of a persons home and work.  
+Read data from a csv file with the following columns: (posix time, utc offset, latitude, longitude, motion state).
+Motion state is a enum with these possible values: 
+Stationary = 0
+Walking = 1
+Running = 2
+Vehicular = 3
+
+Attached is an naive & ideal example of someones location history. How would you generate your own history?
+
+##Determine home and work
+To determine the location of a persons home and work, I assume that the place you are stationary most frequently is home
+and the place you are stationary 2nd most frequently is work. I filtered out the non-stationary rows of data (motion_state != 0)
+
+##Result
+Home coords: (37.767588, -122.496692) Golden Gate Park 
+Work coords: (37.331996999999994, -122.02961100000002) 1 Infinite Loop
+
+##Improvement
+The raw data is clean in that the latitude and longtitude measures taken at home and at work are precisely the same. This is not a good representation of the real world full of interferences; it also would not capature small movements within home or work. Therefore, a gaussian noise is added to the stationary data to account for the statistical error. Data are fuzzed according to a given standard deviation.
+
+##Algorithm
+In the process of developing an algorithm for this problem, multiple algorithm has been considered and experienced with.
+1. Create windows of location coordinates so that fuzzed data can be capture and the mean could be the location. This algorithm fails when there is high fuzzing and locations just exceeds the limits of the windows.
+2. Detect entering/leaving home and work can also be challenging because there could also be motion state error during the event.
+3. K-Means Clustering can solve the problem of statistical noise being too big. Experimenting this algorithm with the stationary data, I find that an increase in number of clusters can lead to more accurate predictions. Yet, this method is too sensitive of outlier that can be found in fuzzed data.
+4. Mean-shift Clustering is chosen as the final algorithm for this problem because it is more robust to outliers. With the stationary data, the highest mode is home , and the second highest mode is work.
+
+##Benchmark
+The benchmark for the predictions is their distance from the groud truth: Golden Gate Park and 1 Infinite Loop.
+
+##Final Results
+###Very Low Fuzz , Standard deviation : 0.5 meters
+Predicted Home coords: [  37.76758799 -122.49669198] (error: 0.00204839260291 meters)
+Predicted Work coords: [  37.33199716 -122.02961107] (error: 0.0191743861948 meters)
+
+###Low Fuzz, Standard deviation: 3 meters
+Predicted Home coords: [  37.76758797 -122.4966914 ] (error: 0.0672033832141 meters)
+Predicted Work coords: [  37.33199608 -122.02960987] (error: 0.156302615043 meters)
+
+###Moderate Fuzz, Standard deviation: 10 meters
+Predicted Home coords: [  37.76759057 -122.49669073] (error: 0.318652627125 meters)
+Predicted Work coords: [  37.33199108 -122.02961107] (error: 0.658203211494 meters)
+
+###High Fuzz, Standard deviation: 1 kilometer
+Predicted Home coords: [  37.76758605 -122.49628467] (error: 45.1877835674 meters)
+Predicted Work coords: [  37.33285503 -122.02925269] (error: 102.401847787 meters)
+
+###Very High Fuzz, Standard deviation: 10 kilometers
+Predicted Home coords: [  37.76367262 -122.5006027 ] (error: 614.659563725 meters)
+Predicted Work coords: [  37.34009755 -122.03065612] (error: 907.26764553 meters)
+
+##Conclusion
+Errors of predictions are acceptable for all level of fuzzing. From very low to moderate, the results are very precise. At high, the result is accept for navigation on Maps. Even at very high level fuzzing, the result is reasonably accurate.
+
+For future improvement, statisical error can also be introduced to the motion state as there too could be errors. It is would be an interesting investigation to see how well this model performs with fuzzed motion state data.
+
+
+
+
+
+
+  
+
+
+
+
+
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from dateutil import tz
 import math
 import numpy as np 
 import pandas as pd
-
-import matplotlib.pyplot as plt
 from sklearn.cluster import MeanShift
 
+
 DATAFILE = "data/location_stream.csv"
+
+
 
 def load_dataset(filepath=DATAFILE):
 	raw_data =  read_csv(filepath, names=
@@ -34,31 +35,37 @@ def load_dataset(filepath=DATAFILE):
 	return format_data
 
 
-#class Solution:
 def determine_home_and_work(data):
-		# Input: (DataFrame) Rows of timestamp/locations/motion_state
-		# Output: ((Float, Float), (Float, Float)) Lat/Long of home and work
-		#Assumptions:
-		#The place you are stationary most frequently is home.
-		#The place you are stationary 2nd most frequently is work
+	# Input: (DataFrame) Rows of timestamp/locations/motion_state
+	# Output: ((Float, Float), (Float, Float)) Lat/Long of home and work
+	#Assumptions:
+	#The place you are stationary most frequently is home
+	#The place you are stationary 2nd most frequently is work
 
-		# Multi-column frequency count 
-		filtered_df = data[data["motion_state"] == 0]
-		agg_df = filtered_df.groupby(["latitude","longitude"]).size().reset_index(name='counts')
-		agg_df = agg_df.sort_values(by=["counts"], ascending=False).reset_index(drop=True)
+	# Multi-column frequency count 
+	filtered_df = data[data["motion_state"] == 0]  #Stationary data
+	agg_df = filtered_df.groupby(["latitude","longitude"]).size().reset_index(name='counts')
+	agg_df = agg_df.sort_values(by=["counts"], ascending=False).reset_index(drop=True)
 
-		home_lat = agg_df.iloc[0]["latitude"]
-		home_long = agg_df.iloc[0]["longitude"]
-		work_lat = agg_df.iloc[1]["latitude"]
-		work_long = agg_df.iloc[1]["longitude"]
+	#Top Frequency count is home
+	home_lat = agg_df.iloc[0]["latitude"]
+	home_long = agg_df.iloc[0]["longitude"]
 
-		return (home_lat, home_long), (work_lat, work_long)
+	#2nd most Frequent is work
+	work_lat = agg_df.iloc[1]["latitude"]
+	work_long = agg_df.iloc[1]["longitude"]
+
+	return (home_lat, home_long), (work_lat, work_long)
 
 
 def meters_to_coord(meters):
+	#Quick evaluation of length to latitude/longitude 
 	return meters/111111.0
 
+
 def distance_between_coords(coord1, coord2):
+	# Input: ((Float, Float), (Float, Float)) Lat/Long Coords
+	# Output: (Float) Distance between two coords 
 	R = 6371
 	delta_lat = (coord2[0] - coord1[0]) * (math.pi/180.)
 	delta_long = (coord2[1] - coord1[1]) * (math.pi/180.)
@@ -71,19 +78,32 @@ def distance_between_coords(coord1, coord2):
 	
 
 def fuzz_data(df,std):
-	# std: standard deviations in meters
+	# Input:((DataFrame), Float) Rows of timestamp/locations/motion_state and standard deviations in meters
+	# Output: (DataFrame) Rows of timestamp/locations/motion_state with locations data fuzzed
+	#Assumptions: 
+	#Location Sensing data are not static in real-life. 
+	#Fuzzing will simulate the statistical error.
+
+	#Convert the unit of standard deviation from meter to coord
 	fuzz_magnitude = meters_to_coord(std)
+
+	#Add Gaussian Noise
 	df["latitude"] = [x + np.random.normal(scale=fuzz_magnitude) for x in df["latitude"]]
 	df["longitude"] = [x + np.random.normal(scale=fuzz_magnitude) for x in df["longitude"]]
+	
 	return df
 
 
 def evaluate(guess, truth):
+	# Input: ((Float, Float), (Float, Float)) Lat/Long Coords of ML Prediction and Ground Truth
+	# Output: (Float) Distance Error between ML Prediction and Groud Truth in meters
 	error = math.sqrt((guess[0]-truth[0])**2 + (guess[1]-truth[1])**2)
 	return 111111.0 * error
 
 def predict(data):
-
+	# Predict home/work with Mean Shift Clustering Algorithm
+	# Input: (DataFrame) Rows of timestamp/locations/motion_state
+	# Output: ((Float, Float), (Float, Float)) home prediction and work prediction
 	X = data["latitude"].tolist()
 	Y = data["longitude"].tolist()
 
@@ -94,7 +114,6 @@ def predict(data):
 
 	model = MeanShift().fit(df)
 	centroids = model.cluster_centers_
-	centroids_norm = [(x * x_std + x_mean, y * y_std + y_mean) for x,y in centroids]
 
 	counts = {}
 	for label in model.labels_:
@@ -105,23 +124,30 @@ def predict(data):
 	work_guess = centroids[counts_sorted[1][0]]
 
 	return home_guess, work_guess
-	
+
 
 def main():
 	data = load_dataset()
 
+	#Determine true coords from original dataset
 	home_coord, work_coord = determine_home_and_work(data)
+	print "Ground Truths"
 	print "Home coords: {}".format(home_coord)
 	print "Work coords: {}".format(work_coord)
 
-	data_fuzzed = fuzz_data(data, 1000)
+	#Fuzz the location
+	data_fuzzed = fuzz_data(data, 10000) #tune level of fuzz: 0.5/3/10/1000/10000
+	data_fuzzed_filtered = data_fuzzed[data_fuzzed["motion_state"] == 0] # Stationary Data
 
-	data_fuzzed_filtered = data_fuzzed[data_fuzzed["motion_state"] == 0]
-
+	#Predict with MeanShift ML Model
 	home_guess, work_guess = predict(data_fuzzed_filtered)
 
+	#Print test results
+	print "Predictions and Errors"
 	print "Predicted Home coords: {} (error: {} meters)".format(home_guess, distance_between_coords(home_coord,home_guess))
 	print "Predicted Work coords: {} (error: {} meters)".format(work_guess, distance_between_coords(work_coord,work_guess))
 
 if __name__ == "__main__":
 	main()
+
+	
